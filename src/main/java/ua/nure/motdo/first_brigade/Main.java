3f25cb86d717e0469106d9b8df74673b37383c01
Denys <denisdenis200524@gmail.com>
2024-03-28 12:05:23 +0200

    added bounded version of newtonRaphson method

--- src/main/java/ua/nure/motdo/first_brigade/Main.java
+++ src/main/java/ua/nure/motdo/first_brigade/Main.java
@@ -3,26 +3,29 @@ package ua.nure.motdo.first_brigade;
 import java.util.function.Function;
 
 public class Main {
+    public static Function<Double, Double> function;
     public static Function<Double, Double> firstDerivative;
     public static Function<Double, Double> secondDerivative;
 
     public static void main(String[] args) {
 //        1 function
+//        function = (x) -> 3 * Math.pow(x, 4) + Math.pow(x - 1, 2);
 //        firstDerivative = (x) -> 12 * Math.pow(x, 3) + 2 * (x - 1);
 //        secondDerivative = (x) -> 36 * Math.pow(x, 2) + 2;
-//        double resultPoint = findMinimumOfFunc(4, 0.0001);
-
+//        double resultPoint = newtonRaphsonMethod(0, 4, 0.0001);
+//
 //        System.out.println(resultPoint);
 
 //        2 function
+        function = (x) -> x / Math.log(x);
         firstDerivative = (x) -> (Math.log(x) - 1) / (Math.pow(Math.log(x), 2));
         secondDerivative = (x) -> -((Math.log(x) - 2) / (x * Math.pow(Math.log(x), 3)));
-        double resultPoint = findMinimumOfFunc(4, 0.0001);
+        double resultPoint = newtonRaphsonMethod(2, 4, 0.0001);
 
-        //System.out.println(resultPoint);// TODO: implement bounded newton method
+        System.out.println(resultPoint);
     }
 
-    public static double findMinimumOfFunc(double startPoint, double accuracy) {
+    public static double newtonRaphsonMethod(double startPoint, double accuracy) {
         double x = startPoint;
         double nextX;
         do {
@@ -35,10 +38,24 @@ public class Main {
         return nextX;
     }
 
+    public static double newtonRaphsonMethod(double startIntervalPoint, double endIntervalPoint, double accuracy) {
+        if ((calculateFunctionInPoint(startIntervalPoint) * calculateSecondDerivativeInPoint(startIntervalPoint)) > 0) {
+            return newtonRaphsonMethod(startIntervalPoint, accuracy);
+        }
+        else if ((calculateFunctionInPoint(endIntervalPoint) * calculateSecondDerivativeInPoint(endIntervalPoint)) > 0) {
+            return newtonRaphsonMethod(endIntervalPoint, accuracy);
+        }
+        else {
+            throw new RuntimeException("Error in intervals");
+        }
+    }
+
+    public static double calculateFunctionInPoint(double x) {
+        return function.apply(x);
+    }
     public static double calculateFirstDerivativeInPoint(double x) {
         return firstDerivative.apply(x);
     }
-
     public static double calculateSecondDerivativeInPoint(double x) {
         return secondDerivative.apply(x);
     }
